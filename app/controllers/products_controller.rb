1ae85c553268b536d5762f7118e012cc739ff795
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 03:31:20 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductsController < ActionController::Base
+class ProductsController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
